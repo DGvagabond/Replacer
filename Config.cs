2a0545ef91e974438b0c2744a7e7a4181042054a
DGvagabond <54526517+DGvagabond@users.noreply.github.com>
2020-08-24 21:51:59 -0500

    Removed unneeded config options

--- Config.cs
+++ Config.cs
@@ -8,11 +8,5 @@ namespace Replacer
     {
         [Description("Wether or not the plugin is enabled.")]
         public bool IsEnabled { get; set; } = true;
-
-        [Description("Do you want to replace players that were kicked?")]
-        public bool ReplaceKicked { get; set; } = true;
-
-        [Description("Do you want to replace players that were banned?")]
-        public bool ReplaceBanned { get; set; } = true;
     }
 }
